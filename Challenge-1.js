0daa9627897fcbab48ff8322d83bbb8f38d0e5a5
Brian Omondi <brianhilsden@gmail.com>
2024-03-22 18:41:06 +0300

    added comments

--- Challenge-1.js
+++ Challenge-1.js
@@ -1,7 +1,8 @@
 const prompt=require('prompt-sync')({sigint:true}); //Calls prompt-sync function
 
-//Function to return the grade based on marks
+/*Function takes user input and returns grade*/
 function gradeCalculator(marks){
+    //Conditionals to check range in which marks lies, then returns grade
     if(marks>79){
         return "A"
     }
@@ -19,27 +20,27 @@ function gradeCalculator(marks){
     }
 }
 
-console.log("Welcome to our grade calculator.\n")
-let exit=false; //declares exit variable and assigns an initial boolean of false to it
+console.log("Welcome to our grade calculator.\n") //Welcome the user
+let exit=false; //Declares exit variable and assigns an initial boolean of false to it
 
 //The while loop continuosly calculates grades until the user choses to exit
 while(exit===false){
     console.log("You can exit at any time by entering 200 ") //Informs user the option to exit
-    const marks=parseInt(prompt("Enter your marks: "))//stores user input as number in marks variable
+    const marks=parseInt(prompt("Enter your marks: "))//Stores user input as integer in a variable
    
     //Implements the exit option
     if(marks===200){  
-        console.log("\n   Thank you for using our calculator! Goodbye.")
-        exit=true    //If user input is 200, the loop is stopped
+        console.log("\n   Thank you for using our calculator! Goodbye.") //Goodbye message
+        exit=true    //Alters exit variable hence stopping the loop/program
     }
 
     //Checks if marks are in the allowed range, passes them into gradeCalculator and displays grade
     else if(marks>=0 && marks<=100){
-        const grade=gradeCalculator(parseInt(marks)) 
+        const grade=gradeCalculator(marks) 
         console.log(`Your grade is: ${grade} \n`)    
     }
 
-    //If number is not in allowed range, informs the user
+    //Informs the user if number is not in allowed range
     else{
         console.log("Invalid entry. Marks should be between 0 and 100 \n")
     }
--- Challenge-2.js
+++ Challenge-2.js
@@ -1,12 +1,15 @@
 const prompt=require('prompt-sync')({sigint:true}); //calls prompt-sync function
 
-//Speed detector function
+//Funtion takes user speed and returns appropriate response depending on the value
 function speedDetector(speed){
-    if(speed<=70){
-        return "Ok \n"
+    //Conditionals to check if speed is above speed limit
+    if(speed<=70){ 
+        return "Ok \n" 
     }
     else{
-        const demeritPoints=(speed-70)/5;
+        const demeritPoints=(speed-70)/5; //Calculate demerit points based on speed, speed limit,rate
+
+        //Conditionals to check if demerit points are above 12 and return appropriate response
         if(demeritPoints>12){
             return "License suspended \n";
         }
@@ -16,25 +19,26 @@ function speedDetector(speed){
     }
 }
 
-console.log("Welcome to our speed calculator.\n")
+console.log("Welcome to our speed calculator.\n") //Welcomes user
 let exit=false; //declares exit variable and assigns an initial boolean of false to it
+
 //The while loop continuosly calculates grades until the user choses to exit
 while(exit===false){
-    console.log("You can exit anytime by entering 800") //Informs user the option to exit
-    const speed=parseInt(prompt("Enter the speed: ")) //stores user input as number in marks variable
+    console.log("You can exit anytime by entering 800") //Informs user about the option to exit
+    const speed=parseInt(prompt("Enter the speed: ")) //Stores user input as integer in a variable
 
     //Implements the exit option
     if(speed===800){
-        console.log("Thank you for using our service! Goodbye\n")
-        exit=true
+        console.log("Thank you for using our service! Goodbye\n") //Goodbye message
+        exit=true //Alters value of exit variable hence stopping loop/program
     }
 
     //Passes the speed into the speedDetector function and displays the result
-    else if(speed>0 && speed<1000000){ //Gave it a speed range so user has to input a number
+    else if(speed>0 && speed<1000000){ //Speed range so user has to input a number
         console.log(speedDetector(speed))
     }
 
-    //User is informed is speed is not in the speed range or not a number
+    //Inform user if speed is not in the speed range or not a number
     else{
         console.log("Kindly ender a valid speed \n")
     }
